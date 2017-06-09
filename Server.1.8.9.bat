24348a551ce812673503bb73a46355c920f605b7
Happypig375 <hadrianwttang@outlook.com>
2017-06-09 21:35:15 +0800

    Played map: The_Prismarine_Lab_v1.2

---
+++ Server.1.8.9.bat
@@ -0,0 +1,6 @@
+echo Initializing...
+title Minecraft Server
+x:
+cd "Dropbox\Minecraft Server"
+java -d64 -Xms1024M -Xmx2048M -jar minecraft_server.1.8.9.jar nogui -o true
+pause
--- Server.bat
+++ Server.bat
@@ -2,5 +2,5 @@ echo Initializing...
 title Minecraft Server
 x:
 cd "Dropbox\Minecraft Server"
-java -d64 -Xms1024M -Xmx2048M -jar minecraft_server.1.11.2.jar nogui -o true
+java -d64 -Xms1024M -Xmx2048M -jar minecraft_server.1.12.jar nogui -o true
 pause
